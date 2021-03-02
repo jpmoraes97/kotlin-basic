2578403178a4acab10495e19feefb0cd389bfa63
João Pedro <joao.moraes@capela.com.br>
2021-03-02 11:53:58 -0300

    Variáveis e Sintaxe

---
+++ src/main/kotlin/VariaveisESintaxe.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+    println("###### Variaveis e Sintaxe ######")
+
+    val x = 0 // criação com atribuição de valor!!
+    val y: Int // quando não atribuimos valor!!
+    val z = "" // inicia nova váriavel com o tipo string!!
+
+    val a: Int
+    val b: String
+    val c: Double
+    val d: Float
+
+    a = 25 // Posso atribuir valor a uma (val) quando a mesma for criada e nenhum valor for atribuido.
+    println("a: $a")
+
+}

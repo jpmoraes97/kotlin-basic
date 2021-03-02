6295af1834d9561a7b5f3900ddb2ad09ef7593a7
João Pedro <joao.moraes@capela.com.br>
2021-03-02 13:42:32 -0300

    Operadores de Compração

---
+++ src/main/kotlin/OperadoresDeComparacao.kt
@@ -0,0 +1,23 @@
+fun main(args: Array<String>) {
+    println("###### Operadores de Comparação ######")
+
+    /*
+     * ==
+     * !=
+     * >
+     * <
+     * >=
+     * <=
+     * is
+     */
+
+    val x = 10
+    val y = 23
+
+    println(x == y)
+    println(x != y)
+    println(x > y)
+    println(x < y)
+    println(x >= y)
+    println(x <= y)
+}

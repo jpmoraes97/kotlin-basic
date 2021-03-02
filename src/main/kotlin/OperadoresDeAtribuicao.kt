4a78a1b9eb55acbed014fc591289731ba140348e
João Pedro <joao.moraes@capela.com.br>
2021-03-02 13:50:51 -0300

    Operadores de Atribuição

---
+++ src/main/kotlin/OperadoresDeAtribuicao.kt
@@ -0,0 +1,26 @@
+fun main(args: Array<String>) {
+    println("###### Operadores de Atribuição ######")
+
+    /*
+     * +=
+     * -=
+     * /=
+     * *=
+     */
+
+    var x = 2
+    x += 2
+    println("$x")
+
+    var y = 10
+    y -= 5
+    println("$y")
+
+    var z = 200
+    z /= 2
+    println("$z")
+
+    var xyz = 200
+    xyz *= 2
+    println("$xyz")
+}

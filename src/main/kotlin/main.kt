bb8e85d342ca0b41c471e409fb81aebfc3e9bae7
João Pedro <joao.moraes@capela.com.br>
2021-03-02 11:40:35 -0300

    Fundamentos

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,11 @@
 fun main(args: Array<String>) {
     println("Hello World!")
+
+    System.out.println("Hello World!!")
+
+    var idade = 23 // mutável durante a execução do programa
+    val pi = 3.14 // não pode ser alterada durante a execução do programa
+
+    println("idade: $idade")
+    println("PI: $pi")
 }

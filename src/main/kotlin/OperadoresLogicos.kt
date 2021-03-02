01d229b9bfb0ea560395b9667f8b0e53b868ceb7
João Pedro <joao.moraes@capela.com.br>
2021-03-02 13:32:22 -0300

    Operadores Lógicos

---
+++ src/main/kotlin/OperadoresLogicos.kt
@@ -0,0 +1,18 @@
+fun main(args: Array<String>) {
+
+    /*
+      != = Verifica se é diferente de
+      == = Verifica se é igual a
+       ! = Inversão de estado
+       is = (instanceof do Javinha) Verifica se um tipo é igual a
+
+     */
+
+    val x = 2
+    val y = 4
+
+    println(x != y)
+    println(x == y)
+    println(!(x != y))
+    println(x is Int)
+}
